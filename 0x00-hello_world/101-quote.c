f1735a95e760e53f0861398cb218995d6a0ddf94
Hicham Mouadili <hicham.mouadli@gmail.com>
2023-07-13 15:50:14 -0700

    print without using printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,23 @@
 #include <string.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
+ * Description: Prints the message "and that piece of art is useful"
+ *              - Dora Korpar, 2015-10-19 to the standard error.
  *
- * Return: Always 0 (Success)
+ * Return: Always 1.
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, "
-	"2015-10-19\n";
-	
-	write(2, message, strlen(message));
+	const char *message = "and that piece of art is useful\" - Dora Korpar, ";
+	const char *date = "2015-10-19\n";
+	int message_len = strlen(message);
+	int date_len = strlen(date);
+
+	write(2, message, message_len);
+	write(2, date, date_len);
+
 	return (1);
 }
+
