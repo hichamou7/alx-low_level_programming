eabce5f8add20d33ba60539d934d27cc0ac7534d
Hicham Mouadili <hicham.mouadli@gmail.com>
2023-07-14 17:03:51 -0700

    print combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,19 +10,15 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		putchar(i + '0');
+		if (i != 9)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i != 8 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
