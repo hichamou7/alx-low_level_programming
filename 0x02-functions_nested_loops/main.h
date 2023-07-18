aa23df8ee23b5f2d7d0e373472ebbedea025812a
Hicham Mouadili <hicham.mouadli@gmail.com>
2023-07-18 12:16:57 -0700

    add the main.h file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - Writes a character to the standard output
+ *
+ * @c: The character to be written
+ *
+ * Return: On success, the character written
+ *         On error, -1 is returned
+ */
+int _putchar(char c);
+
+#endif /* MAIN_H */
