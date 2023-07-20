658e313fa3ae2694e34ca45cf7be103dbaa66bc0
Hicham Mouadili <hicham.mouadli@gmail.com>
2023-07-20 16:25:55 -0700

    fizz buzz code

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Prints numbers from 1 to 100 with Fizz, Buzz, or FizzBuzz conditions
+ *
+ * Description: This program prints the numbers from 1 to 100, inclusive.
+ *              However, for multiples of three, it prints "Fizz" instead
+ *              of the number. For multiples of five, it prints "Buzz."
+ *              For numbers that are multiples of both three and five,
+ *              it prints "FizzBuzz." All output is separated by tabs.
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	for (int i = 1; i <= 100; i++)
