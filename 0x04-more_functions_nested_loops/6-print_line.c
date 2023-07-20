a9d9cac94f7dce7c8dbb25b186fd8641adf0eb68
Hicham Mouadili <hicham.mouadli@gmail.com>
2023-07-20 16:07:06 -0700

    draw a straight linex

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * print_line - Draws a straight line in the terminal.
- * @n: The number of times the character '_' should be printed.
+ * print_line - Draws a straight line in the terminal using '_'
+ * @n: The number of times the character '_' should be printed
  *
- * The line ends with a '\n'.
- * If n is 0 or less, the function should only print '\n'.
+ * Description: This function prints a line of '_' characters in the terminal.
+ *              The number of characters to be printed is specified by 'n'.
+ *              If 'n' is 0 or less, the function prints only a newline.
  */
 
 void print_line(int n)
@@ -13,13 +14,14 @@ void print_line(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-
-	for (int i = 0; i < n; i++)
+	else
 	{
-		_putchar('_');
+		while (n > 0)
+		{
+			_putchar('_');
+			n--;
+		}
+		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
