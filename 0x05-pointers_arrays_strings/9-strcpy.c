d302d003a287aa68e6112059fef7258172e06d06
Hicham Mouadili <hicham.mouadli@gmail.com>
2023-07-24 16:22:58 -0700

    copy str from src to dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _strcpy - Copies the string pointed to by src to the buffer pointed to by dest.
+ * _strcpy - Copies the string pointed to by src to the buffer 
+ * pointed to by dest.
  * @dest: Pointer to the destination buffer.
  * @src: Pointer to the source string to be copied.
  * Return: Pointer to dest.
