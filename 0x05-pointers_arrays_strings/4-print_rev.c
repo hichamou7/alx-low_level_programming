50961a34f059509922117239cc2450197d39069d
Hicham Mouadili <hicham.mouadli@gmail.com>
2023-07-24 15:37:37 -0700

    print a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,14 @@ void print_rev(char *s)
 	if (s != NULL)
 	{
 		int length = 0;
+		int i;
 
 		/* Calculate the length of the string */
 		while (s[length] != '\0')
 			length++;
 
 		/* Print the string in reverse order */
-		for (int i = length - 1; i >= 0; i--)
+		for (i = length - 1; i >= 0; i--)
 			_putchar(s[i]);
 
 		_putchar('\n');
