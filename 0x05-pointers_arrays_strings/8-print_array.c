6537f9fc307bbdb41132d394e71972dbe49a623a
Hicham Mouadili <hicham.mouadli@gmail.com>
2023-07-24 16:19:51 -0700

    priint n elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints n elements of an array of integers, followed by a new line.
+ * print_array - Prints n elements of an array of integers,
+ * followed by a new line.
  * @a: Pointer to the array of integers.
  * @n: Number of elements to be printed.
  */
