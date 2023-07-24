8ae2552debc6453f0df0e6280766ec50607cf447
Hicham Mouadili <hicham.mouadli@gmail.com>
2023-07-24 16:22:01 -0700

    copy str from src to dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,23 @@
 #include "main.h"
 
 /**
- * _isupper - Checks if a character is uppercase.
- * @c: The character to be checked.
- *
- * Return: 1 if c is uppercase, 0 otherwise.
+ * _strcpy - Copies the string pointed to by src to the buffer pointed to by dest.
+ * @dest: Pointer to the destination buffer.
+ * @src: Pointer to the source string to be copied.
+ * Return: Pointer to dest.
  */
 
-int _isupper(int c)
+char *_strcpy(char *dest, char *src)
 {
-	return ((c >= 'A' && c <= 'Z') ? 1 : 0);
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
