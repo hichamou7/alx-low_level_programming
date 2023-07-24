22f650c9292844ff0267f3e03d93bafa57d23715
Hicham Mouadili <hicham.mouadli@gmail.com>
2023-07-24 15:23:09 -0700

    create all files + main.h + first script to update the value of a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value an integer points to 98.
+ * @n: Pointer to the integer to be updated.
+ */
+
+void reset_to_98(int *n)
+{
+	if (n != NULL)
+	{
+		*n = 98;
+	}
+}
