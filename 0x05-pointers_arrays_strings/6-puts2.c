269a05ec0837806c6fd880516c0fa3e420022fe4
Hicham Mouadili <hicham.mouadli@gmail.com>
2023-07-24 15:46:14 -0700

    print every other caracter

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,6 +11,14 @@ void puts2(char *str)
 	{
 		int i = 0;
 
+		/* Remove null-terminator from the string */
+		while (str[i] != '\0')
+			i++;
+
+		str[i - 1] = '\0'; // Remove the null-terminator
+
+		/* Print every other character */
+		i = 0;
 		while (str[i] != '\0')
 		{
 			_putchar(str[i]);
