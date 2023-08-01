bed33c0c6e07ea7038f777a7be9f02e5acacf548
Hicham Mouadili <hicham.mouadli@gmail.com>
2023-08-01 15:01:26 -0700

    Write a function that sets the value of a pointer to a char.

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,28 +1,9 @@
-#include "main.h"
-
 /**
- * rot13 - Encodes a string using rot13.
- * @str: Pointer to the string to be encoded.
- * Return: Pointer to the encoded string.
+ * set_string - Sets the value of a pointer to a char.
+ * @s: Pointer to the pointer to be set.
+ * @to: Pointer to the char to be assigned to the pointer.
  */
-char *rot13(char *str)
+void set_string(char **s, char *to)
 {
-	char *rot13_lower = "abcdefghijklmnopqrstuvwxyz";
-	char *rot13_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char *rot13_shifted = "nopqrstuvwxyzabcdefghijklm";
-	int i, j;
-	int is_letter;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		is_letter = (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z');
-		j = is_letter ? (str[i] & 32) ? str[i] - 'a' : str[i] - 'A' : 0;
-
-		if (is_letter)
-		{
-			str[i] = (str[i] & 32) ? rot13_shifted[j] - 'a' + 'A' : rot13_shifted[j];
-		}
-	}
-
-	return (str);
+	*s = to;
 }
