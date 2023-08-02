133a531f9d2ae2491afa96160dd82bb58f7ec663
Hicham Mouadili <hicham.mouadli@gmail.com>
2023-08-02 11:25:07 -0700

    Write a function that returns 1 if the input integer is a prime number, otherwise return 0.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - Returns the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s)
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
+
+/**
  * is_palindrome - Checks if a string is a palindrome.
  * @s: The input string.
  *
@@ -8,39 +27,13 @@
  */
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion(s);
+	int len = _strlen(s);
 
-	return (check_palindrome(s, len));
-}
-
-/**
- * _strlen_recursion - Calculates the length of a string.
- * @s: The input string.
- *
- * Return: The length of the string.
- */
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
-}
-
-/**
- * check_palindrome - Helper function to check if a string
- *	is a palindrome.
- * @s: The input string.
- * @len: The length of the string.
- *
- * Return: 1 if the string is a palindrome, 0 otherwise.
- */
-int check_palindrome(char *s, int len)
-{
 	if (len <= 1)
 		return (1);
 
-	if (*s != s[len - 1])
+	if (*s != *(s + len - 1))
 		return (0);
 
-	return (check_palindrome(s + 1, len - 2));
+	return (is_palindrome(s + 1));
 }
