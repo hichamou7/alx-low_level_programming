8380171e9cd098b6c8ac1719bf8eb62b84b91fcf
Hicham Mouadili <hicham.mouadli@gmail.com>
2023-08-02 11:14:26 -0700

    Write a function that returns 1 if the input integer is a prime number, otherwise return 0.

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,19 +1,33 @@
 #include "main.h"
 
 /**
- * print_chessboard - Prints the chessboard.
- * @a: 2D array representing the chessboard.
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to check.
+ *
+ * Return: 1 if the number is prime, 0 otherwise.
  */
-void print_chessboard(char (*a)[8])
+int is_prime_number(int n)
 {
-	int i, j;
+	return (check_prime(n, 2));
+}
 
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
-		_putchar('\n');
-	}
+/**
+ * check_prime - Helper function to check if a number is prime.
+ * @n: The number to check.
+ * @i: The current divisor to test.
+ *
+ * Return: 1 if the number is prime, 0 otherwise.
+ */
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+
+	if (i * i > n)
+		return (1);
+
+	if (n % i == 0)
+		return (0);
+
+	return (check_prime(n, i + 1));
 }
