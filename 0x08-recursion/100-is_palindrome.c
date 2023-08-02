c4133b84803efffed667edc0ef329e7dfa1579bb
Hicham Mouadili <hicham.mouadli@gmail.com>
2023-08-02 11:19:27 -0700

    Write a function that returns 1 if a string is a palindrome and 0 if not.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,9 +1,46 @@
+#include "main.h"
+
 /**
- * set_string - Sets the value of a pointer to a char.
- * @s: Pointer to the pointer to be set.
- * @to: Pointer to the char to be assigned to the pointer.
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The input string.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
  */
-void set_string(char **s, char *to)
+int is_palindrome(char *s)
 {
-	*s = to;
+	int len = _strlen_recursion(s);
+
+	return (check_palindrome(s, len));
+}
+
+/**
+ * _strlen_recursion - Calculates the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_palindrome - Helper function to check if a string
+ *	is a palindrome.
+ * @s: The input string.
+ * @len: The length of the string.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
+int check_palindrome(char *s, int len)
+{
+	if (len <= 1)
+		return (1);
+
+	if (*s != s[len - 1])
+		return (0);
+
+	return (check_palindrome(s + 1, len - 2));
 }
